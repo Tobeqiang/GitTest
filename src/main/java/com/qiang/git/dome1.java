f62fec978a1e8f9c84dfbbb1e5f57a11c613e0dd
Tobe <1103713421@qq.com>
2022-03-06 11:20:47 +0800

    third commit

--- src/main/java/com/qiang/git/dome1.java
+++ src/main/java/com/qiang/git/dome1.java
@@ -4,5 +4,6 @@ public class dome1 {
     public static void main(String[] args) {
         System.out.println("666666");
         System.out.println("777777");
+        System.out.println("888888");
     }
 }
